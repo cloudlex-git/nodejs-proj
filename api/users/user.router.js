5bade2d0587a33b143459ce77a504c10ef2685f4
cloudlex-git <121755249+cloudlex-git@users.noreply.github.com>
2023-01-02 12:24:14 +0800

    Add files via upload

---
+++ api/users/user.router.js
@@ -0,0 +1,17 @@
+const { 
+    createUser,
+    getUserByUserId,
+    getUsers,
+    updateUsers,
+    deleteUser,
+    login 
+} = require("./user.controller");
+const router = require("express").Router();
+
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserByUserId);
+router.patch("/", updateUsers);
+router.delete("/", deleteUser);
+router.post("/login", login);
+module.exports = router;
